1e0db36dd270629a023177c67a6db57820cdc1b3
shashank <shashankhosamani1109@gmail.com>
2024-03-05 15:42:27 +0530

    Describe this commit

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World!\n";
+  cout << "Hello, Jenkins!\n";
+  cout << "PES1UG21CS557 Here\n";
+  cout << "Task 4 completed sucessfully! \n";
+  cout << "I have successfully built and run.\n";
+  return 0;
+}
